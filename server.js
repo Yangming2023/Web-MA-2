05719cae6b464f9a3a1e89fe3a85046ea82c2b0a
Louis Cheng <145710569+Yangming2023@users.noreply.github.com>
2026-02-05 23:27:22 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -7,19 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json()); 
 app.use(express.static('public')); // This serves your HTML automatically!
-let dataStore = [{ name: "Teacher", message: "API is live!" }];
 
-// GET: Send data from the datastore to the user
-app.get('/api/messages', (req, res) => {
-    res.json(dataStore);
-});
 
-// POST: Take data sent and store it in the datastore
-app.post('/api/messages', (req, res) => {
-    dataStore.push(req.body);
-    res.status(201).send({ message: "Received!" });
-});
-
-app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
-
-// Reminder: node server.js in terminal to start it running
